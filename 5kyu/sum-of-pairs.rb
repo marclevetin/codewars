5acd6c5abb4813382d5531544dd73e64d985dd41
marclevetin <marc@derbyhat.org>
2017-07-05 13:04:46 -0400

    save point.  Sum of Pairs needs a function to convert indices to values and to find the best answer

---
+++ 5kyu/sum-of-pairs.rb
@@ -0,0 +1,58 @@
+# https://www.codewars.com/kata/54d81488b981293527000c8f/train/rubySum of Pairs
+#
+# Given a list of integers and a single sum value, return the first two values
+# (parse from the left please) in order of appearance that add up to form the
+# sum.
+#
+# sum_pairs([11, 3, 7, 5],         10)
+# #              ^--^      3 + 7 = 10
+# == [3, 7]
+#
+# sum_pairs([4, 3, 2, 3, 4],         6)
+# #          ^-----^         4 + 2 = 6, indices: 0, 2 *
+# #             ^-----^      3 + 3 = 6, indices: 1, 3
+# #                ^-----^   2 + 4 = 6, indices: 2, 4
+# #  * entire pair is earlier, and therefore is the correct answer
+# == [4, 2]
+#
+# sum_pairs([0, 0, -2, 3], 2)
+# #  there are no pairs of values that can be added to produce 2.
+# == None/nil/undefined (Based on the language)
+#
+# sum_pairs([10, 5, 2, 3, 7, 5],         10)
+# #              ^-----------^   5 + 5 = 10, indices: 1, 5
+# #                    ^--^      3 + 7 = 10, indices: 3, 4 *
+# #  * entire pair is earlier, and therefore is the correct answer
+# == [3, 7]
+# Negative numbers and duplicate numbers can and will appear.
+#
+# NOTE: There will also be lists tested of lengths upwards of 10,000,000
+# elements. Be sure your code doesn't time out.
+
+# pseudocode
+# loop through numbers.
+# For each number, see if the second number exists
+# log both indices
+#
+# exit loop after index is greater than second index of the first pair
+#
+# compare collections of numbers
+# return pair with the lower indices wins
+
+require 'pry'
+def sum_pairs(ints, s)
+  pairs = []
+  ints.each_with_index do |first_number, index|
+    second_number = s - first_number
+    second_number_index = ints.index {|i| i == second_number}
+    if second_number_index && index < second_number_index
+      first_number_index = index
+      array = [first_number_index, second_number_index]
+      pairs << array
+    end
+  end
+binding.pry
+
+end
+
+sum_pairs([1, 4, 8, 7, 3, 15], 8)
